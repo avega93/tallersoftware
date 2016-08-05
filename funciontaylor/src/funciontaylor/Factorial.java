0290bebabbd3f4062ced624612e83eace458fb3d
avega93 <camilo.931107@gmail.com>
2016-08-05 11:46:08 -0500

    modifique funcion factorial

--- funciontaylor/src/funciontaylor/Factorial.java
+++ funciontaylor/src/funciontaylor/Factorial.java
@@ -1,5 +1,15 @@
 package funciontaylor;
 
 public class Factorial {
-
+ static int factorial(int n){
+	          if ( n <= 1 ) {
+	              return 1;
+	          } else {
+	              long factorial = n*factorial(numero-1);
+	        	  return factorial
+	          }
+	     }
 }
+
+	 
+
